750698c065c6482f27b84692d203be90645978a9
Nicolas Devenet <github@devenet.info>
2015-01-13 13:36:53 +0100

    Add Twitter & FB + open in new window

--- scripts/options.js
+++ scripts/options.js
@@ -1,11 +1,18 @@
+var new_window = document.getElementById('new_window');
 var shaarli_url = document.getElementById('shaarli_url');
-if (localStorage['shaarli.url']) {
-  shaarli_url.value = localStorage['shaarli.url'];
-}
-
 var form = document.querySelector('button');
+
 form.innerHTML = chrome.i18n.getMessage('global_save');
+new_window.parentNode.querySelector('span').innerHTML = chrome.i18n.getMessage('settings_open_window');
+
+if (localStorage['settings.shaarli.url']) {
+  shaarli_url.value = localStorage['settings.shaarli.url'];
+}
+if (localStorage['settings.open.window']) {
+  new_window.checked = localStorage['settings.open.window'];
+}
 form.addEventListener("click", function() {
-  localStorage['shaarli.url'] = shaarli_url.value;
+  localStorage['settings.shaarli.url'] = shaarli_url.value;
+  localStorage['settings.open.window'] = new_window.checked;
   return true;
 });
--- scripts/share.js
+++ scripts/share.js
@@ -6,7 +6,9 @@ for (var i=0, l=links.length; i<l; i++) {
   links[i].innerHTML = chrome.i18n.getMessage('share_' + method);
 
   //listener
-  links[i].addEventListener("click", function() {
+  links[i].addEventListener("click", function(event) {
+
+    method = event.path[0].hash.substring(1);
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
       var result;
       var url = tabs[0].url;
@@ -14,21 +16,39 @@ for (var i=0, l=links.length; i<l; i++) {
 
       switch(method) {
         case 'shaarli':
-          if (!localStorage['shaarli.url']) {
+          if (!localStorage['settings.shaarli.url']) {
             alert('Merci de configurer l\'URL du Shaarli.');
             break;
           }
-          result = localStorage['shaarli.url'] + "/?post=" + url
-            + "&title=" + title;
+          result = localStorage['shaarli.url'] + '/?post=' + encodeURI(url)
+            + '&title=' + encodeURI(title);
           break;
 
         case 'email':
-          result = "mailto:?" + "subject=" + title + "&body=" + url;
+          result = 'mailto:?' + 'subject=' + encodeURI(title) + '&body=' + encodeURI(url);
+          break;
+
+        case 'twitter':
+          result = 'https://twitter.com/intent/tweet' + '?text=' + encodeURI(title)
+            + '&url=' + encodeURI(url);
+          break;
+
+        case 'facebook':
+          result = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURI(url)
+            + '&t=' + encodeURI(title);
           break;
       }
 
       if (result) {
-        chrome.tabs.create({url: encodeURI(result)});
+        if (localStorage['settings.open.window']) {
+          chrome.windows.create( {
+            url: result,
+            type: "popup",
+            width: 450,
+            height: 450
+          });
+        }
+        else { chrome.tabs.create({url: result}); }
       }
     });
   });
