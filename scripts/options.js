af3ee4dcbeee3c593088b69cf06c5e7e5c745cad
Nicolas Devenet <github@devenet.info>
2015-01-14 16:46:50 +0100

    Select services to be displayed

--- scripts/options.js
+++ scripts/options.js
@@ -1,25 +1,60 @@
 var new_window = document.getElementById('new_window');
 var shaarli_url = document.getElementById('shaarli_url');
-var form = document.querySelector('button');
+var share_items = document.querySelectorAll('.i18n-item');
+var more_shaarli = document.getElementById('more_shaarli');
+var form_msg = document.getElementById('form_msg');
+var items = { email:1, facebook:0, shaarli:0, twitter:1 };
 
+// translation
 document.querySelector('h1').innerHTML = chrome.i18n.getMessage('settings_name');
-form.innerHTML = chrome.i18n.getMessage('global_save');
+document.querySelector('button').innerHTML = chrome.i18n.getMessage('global_save');
+
+for (var i=0, l=share_items.length; i<l; i++) {
+  share_items[i].querySelector('span').innerHTML = chrome.i18n.getMessage(share_items[i].getAttribute('for'));
+}
 new_window.parentNode.querySelector('span').innerHTML = chrome.i18n.getMessage('settings_open_window');
 
-if (localStorage['settings.shaarli.url']) {
-  shaarli_url.value = localStorage['settings.shaarli.url'];
+// utils
+function loadData() {
+  items = !localStorage['settings.share.items'] ? items : JSON.parse(localStorage['settings.share.items']);
+
+  for (var i=0, l=share_items.length; i<l; i++) {
+    share_items[i].querySelector('input').checked = items[share_items[i].getAttribute('for').substring(6)];
+  }
+
+  more_shaarli.style.display = items['shaarli'] ? 'block' : 'none';
+  shaarli_url.required = items['shaarli'];
+  if (localStorage['settings.shaarli.url']) {
+    shaarli_url.value = localStorage['settings.shaarli.url'];
+  }
+  if (localStorage['settings.open.window']) {
+    new_window.checked = localStorage['settings.open.window'] == "true";
+  }
 }
-if (localStorage['settings.open.window']) {
-  new_window.checked = localStorage['settings.open.window'] == "true";
+
+function saveData() {
+  for (var i=0, l=share_items.length; i<l; i++) {
+    items[share_items[i].getAttribute('for').substring(6)] = share_items[i].querySelector('input').checked;
+  }
+  localStorage['settings.share.items'] = JSON.stringify(items);
+  localStorage['settings.shaarli.url'] = shaarli_url.value;
+  localStorage['settings.open.window'] = new_window.checked;
 }
+
+// load and save data
+document.addEventListener('DOMContentLoaded', loadData);
 document.addEventListener("submit", function(e) {
   e.preventDefault();
 
-  localStorage['settings.shaarli.url'] = shaarli_url.value;
-  localStorage['settings.open.window'] = new_window.checked;
-
-  form.innerHTML = chrome.i18n.getMessage('global_saved');
-  setTimeout(function() { form.innerHTML = chrome.i18n.getMessage('global_save'); }, 3000);
+  saveData();
+  form_msg.textContent = chrome.i18n.getMessage('global_saved');
+  setTimeout(function() { form_msg.textContent = ''; }, 3500);
 
   return false;
+});
+
+// listeners
+document.getElementById('share_shaarli').addEventListener('change', function() {
+  more_shaarli.style.display = this.checked ? 'block' : 'none';
+  shaarli_url.required = this.checked;
 });
--- scripts/share.js
+++ scripts/share.js
@@ -1,59 +1,73 @@
-var links = document.querySelectorAll('li a');
+var items = { email:1, facebook:0, shaarli:0, twitter:1 };
+items = !localStorage['settings.share.items'] ? items : JSON.parse(localStorage['settings.share.items']);
+
+var links = document.querySelectorAll('.i18n-share a');
 for (var i=0, l=links.length; i<l; i++) {
   var method = links[i].getAttribute('href').substring(1);
 
-  // translation
-  links[i].innerHTML = chrome.i18n.getMessage('share_' + method);
+  // item enabled
+  if (items[method]) {
+    // translation
+    links[i].innerHTML = chrome.i18n.getMessage('share_' + method);
 
-  //listener
-  links[i].addEventListener("click", function(event) {
+    //listener
+    links[i].addEventListener("click", function(event) {
+      event.preventDefault();
 
-    method = event.path[0].hash.substring(1);
-    chrome.tabs.query({ active: true }, function (tabs) {
-      var result;
-      var url = tabs[0].url;
-      var title = tabs[0].title;
-      var window_openable = true;
-
-      switch(method) {
-        case 'shaarli':
-          if (!localStorage['settings.shaarli.url']) {
-            alert('Merci de configurer l\'URL du Shaarli.');
-            break;
+      method = event.path[0].hash.substring(1);
+      chrome.tabs.query({ active: true }, function (tabs) {
+        var result = buildShareURL(method, tabs[0].url, tabs[0].title);
+        var window_openable = method != 'email';
+        if (result) {
+          if (window_openable && localStorage['settings.open.window'] == "true") {
+            chrome.windows.create( {
+              url: result,
+              type: "popup",
+              width: 450,
+              height: 450
+            });
           }
-          result = localStorage['shaarli.url'] + '/?post=' + encodeURI(url)
-            + '&title=' + encodeURI(title);
-          break;
-
-        case 'email':
-          result = 'mailto:?' + 'subject=' + encodeURI(title) + '&body=' + encodeURI(url);
-          window_openable = false;
-          break;
-
-        case 'twitter':
-          result = 'https://twitter.com/intent/tweet' + '?text=' + encodeURI(title)
-            + '&url=' + encodeURI(url);
-          break;
-
-        case 'facebook':
-          result = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURI(url)
-            + '&t=' + encodeURI(title);
-          break;
-      }
-
-      if (result) {
-        if (window_openable && localStorage['settings.open.window'] == "true") {
-          chrome.windows.create( {
-            url: result,
-            type: "popup",
-            width: 450,
-            height: 450
-          });
+          else { chrome.tabs.create({url: result}); }
         }
-        else { chrome.tabs.create({url: result}); }
-      }
+      });
+
+      return true;
     });
-  });
+  }
+  // item disabled
+  else {
+    links[i].parentNode.style.display = 'none';
+  }
 }
 
-document.querySelector('p a').innerHTML = chrome.i18n.getMessage('settings_name');
+function buildShareURL(method, url, title) {
+  var result = null;
+  switch(method) {
+    case 'shaarli':
+      if (!localStorage['settings.shaarli.url']) {
+        var share_msg = document.getElementById('share-msg');
+        share_msg.innerHTML = '<p>' + chrome.i18n.getMessage('share_shaarli_missing_url') + ' &#8227; <a href="'
+          + chrome.extension.getURL('options.html')
+          + '" target="_blank">' + chrome.i18n.getMessage('settings_name') + '</a></p>';
+        break;
+      }
+      result = localStorage['shaarli.url'] + '/?post=' + encodeURI(url)
+        + '&title=' + encodeURI(title);
+      break;
+
+    case 'email':
+      result = 'mailto:?' + 'subject=' + encodeURI(title) + '&body=' + encodeURI(url);
+      break;
+
+    case 'twitter':
+      result = 'https://twitter.com/intent/tweet' + '?text=' + encodeURI(title)
+        + '&url=' + encodeURI(url);
+      break;
+
+    case 'facebook':
+      result = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURI(url)
+        + '&t=' + encodeURI(title);
+      break;
+  }
+  return result;
+}
